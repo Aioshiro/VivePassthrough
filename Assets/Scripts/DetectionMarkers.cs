bd5a9166e7a06738246458ab14158cdd9b263ba2
Crenau_Stagiaires Lucas_Detto <ldetto@enseignement.nantes.archi.fr>
2022-09-16 13:55:46 +0200

    Added rotation in camera space

--- Assets/Scripts/DetectionMarkers.cs
+++ Assets/Scripts/DetectionMarkers.cs
@@ -14,22 +14,20 @@ public class DetectionMarkers : MonoBehaviour
 	public Point2f[][] corners;
 	private int[] ids;
 	private Point2f[][] rejectedImgPoints;
-	public Renderer initialImage;
-	public Renderer convertedImage;
-	public Renderer finalImage;
-
 	Texture2D left;
 	Texture2D leftCPU;
 	Texture2D gray;
 
 	Point3f[] markerPoints;
-	private float markerLength=0.06f;
+	private float markerLength = 0.06f;
 	private double[,] cameraMatrix;
 	double[] distCoeffs = new double[4] { 0d, 0d, 0d, 0d };
 	double[,] rotMat = new double[3, 3] { { 0d, 0d, 0d }, { 0d, 0d, 0d }, { 0d, 0d, 0d } };
 	private bool isCameraInitialized;
-	private Matrix4x4 leftPose;
+	public Matrix4x4 leftPose;
 
+	public double[] rvec;
+	public double[] tvec;
 
 	public GameObject cubeToMove;
 
@@ -40,7 +38,7 @@ public class DetectionMarkers : MonoBehaviour
 
 	void Start()
 	{
-		 markerPoints= new Point3f[] {
+		markerPoints = new Point3f[] {
 				new Point3f(-markerLength / 2f,  markerLength / 2f, 0f),
 				new Point3f( markerLength / 2f,  markerLength / 2f, 0f),
 				new Point3f( markerLength / 2f, -markerLength / 2f, 0f),
@@ -64,15 +62,15 @@ public class DetectionMarkers : MonoBehaviour
 	}
 
 	void OnCompleteReadback(AsyncGPUReadbackRequest request)
-    {
-        var tex = new Texture2D(Vive.Plugin.SR.ViveSR_DualCameraImageCapture.UndistortedImageWidth, Vive.Plugin.SR.ViveSR_DualCameraImageCapture.UndistortedImageHeight, TextureFormat.RGBA32, false);
-        tex.LoadRawTextureData(request.GetData<uint>());
-        tex.Apply();
-        Graphics.CopyTexture(tex, leftCPU);
-        Destroy(tex);
-    }
+	{
+		var tex = new Texture2D(Vive.Plugin.SR.ViveSR_DualCameraImageCapture.UndistortedImageWidth, Vive.Plugin.SR.ViveSR_DualCameraImageCapture.UndistortedImageHeight, TextureFormat.RGBA32, false);
+		tex.LoadRawTextureData(request.GetData<uint>());
+		tex.Apply();
+		Graphics.CopyTexture(tex, leftCPU);
+		Destroy(tex);
+	}
 
-    private void Update()
+	private void Update()
 	{
 
 		if (!initialized)
@@ -80,8 +78,8 @@ public class DetectionMarkers : MonoBehaviour
 			InitTextures();
 			initialized = true;
 		}
-		if (!isCameraInitialized  && ViveSR_DualCameraImageCapture.FocalLengthLeft > 0)
-        {
+		if (!isCameraInitialized && ViveSR_DualCameraImageCapture.FocalLengthLeft > 0)
+		{
 			cameraMatrix = new double[3, 3] {
 				{ViveSR_DualCameraImageCapture.FocalLengthLeft, 0d, ViveSR_DualCameraImageCapture.UndistortedCxLeft},
 				{0d, ViveSR_DualCameraImageCapture.FocalLengthLeft, ViveSR_DualCameraImageCapture.UndistortedCyLeft},
@@ -90,50 +88,61 @@ public class DetectionMarkers : MonoBehaviour
 
 			isCameraInitialized = true;
 		}
-		
 
-		if (initialized&& isCameraInitialized && request.done)
+
+		if (initialized && isCameraInitialized && request.done)
 		{
-			initialImage.material.mainTexture = leftCPU;
 			Mat mat = OpenCvSharp.Unity.TextureToMat(leftCPU);
 			Mat flippedMat = new Mat();
 			Cv2.Flip(mat, flippedMat, FlipMode.Y);
 			Mat grayMat = new Mat();
 			Cv2.CvtColor(flippedMat, grayMat, ColorConversionCodes.BGRA2GRAY);
-			gray = OpenCvSharp.Unity.MatToTexture(grayMat);
-
-			convertedImage.material.mainTexture = gray;
 
 			// Detect and draw markers
 			CvAruco.DetectMarkers(grayMat, dictionary, out corners, out ids, detectorParameters, out rejectedImgPoints);
-			CvAruco.DrawDetectedMarkers(flippedMat, corners, ids);
+			//CvAruco.DrawDetectedMarkers(flippedMat, corners, ids);
 
 			if (ids.Length > 0)
-            {
+			{
 				for (int i = 0; i < ids.Length; i++)
 				{
 					if (ids[i] == 0)
 					{
-						Cv2.SolvePnP(markerPoints, corners[i], cameraMatrix, distCoeffs, out double[] rvec, out double[] tvec, false, SolvePnPFlags.Iterative);
+						Cv2.SolvePnP(markerPoints, corners[i], cameraMatrix, distCoeffs, out rvec, out tvec, false, SolvePnPFlags.Iterative);
 						Cv2.Rodrigues(rvec, out rotMat);
+						//rotMat = LHMatrixFromRHMatrix(rotMat);
+
+						Vector3 forward = new Vector3((float) rotMat[2, 0], (float) rotMat[2, 1],(float) rotMat[2, 2]);
+						Vector3 up = new Vector3((float)rotMat[1, 0], (float)rotMat[1, 1], (float)rotMat[1, 2]);
+
+						//forward = leftPose.MultiplyVector(forward);
+						//up = leftPose.MultiplyVector(up);
+
+						Quaternion finalRot = Quaternion.LookRotation(forward, up);
 
 						Vector3 cameraSpacePos = new Vector3(-(float)tvec[0], (float)tvec[1], (float)tvec[2]); //x is negative due to flipping of camera
-						Quaternion cameraSpaceRot = Quaternion.Euler((float) rvec[0], (float) rvec[1], (float) rvec[2]);
 
 						Vector3 worldPos = leftPose.MultiplyPoint(cameraSpacePos);
 
-						cubeToMove.transform.position = worldPos;
-						cubeToMove.transform.rotation = cameraSpaceRot;
+						Quaternion cameraRot = QuaternionFromMatrix(rotMat);
+
+						cameraRot.y = -cameraRot.y;
+						//cameraRot.x = -cameraRot.x;
+						cameraRot.z = -cameraRot.z;
+
+						//float temp = cameraRot.x;
+						//cameraRot.x = cameraRot.z;
+						//cameraRot.z = temp;
+
+						//cubeToMove.SetNewTransform(worldPos, finalRot);
+						cubeToMove.transform.localPosition = cameraSpacePos;
+						cubeToMove.transform.localRotation = cameraRot;
 					}
 				}
 
 
 
 			}
-
-			Texture2D outputTexture = OpenCvSharp.Unity.MatToTexture(flippedMat);
-			finalImage.material.mainTexture = outputTexture;
-
 
 
 			grayMat.Dispose();
@@ -144,8 +153,61 @@ public class DetectionMarkers : MonoBehaviour
 			request = AsyncGPUReadback.Request(left, 0, TextureFormat.RGBA32, OnCompleteReadback);
 		}
 
-
-
 	}
 
+	private double[,] MultiplyMatrix(double[,] m1,double[,] m2)
+    {
+		double[,] res = new double[3, 3];
+		for (int i = 0; i < 3; i++)
+        {
+			for (int j = 0; j < 3; j++)
+            {
+				double sum = 0;
+				for (int k = 0; k < 3; k++)
+                {
+					sum += m1[i, k] * m2[k, j];
+                }
+				res[i, j] = sum;
+            }
+        }
+		return res;
+    }
+
+	private Quaternion QuaternionFromMatrix(double[,] m)
+	{
+		// Adapted from: http://www.euclideanspace.com/maths/geometry/rotations/conversions/matrixToQuaternion/index.htm
+		Quaternion q = new Quaternion
+		{
+			w = Mathf.Sqrt(Mathf.Max(0, (float)(1 + m[0, 0] + m[1, 1] + m[2, 2]))) / 2,
+			x = Mathf.Sqrt(Mathf.Max(0, (float)(1 + m[0, 0] - m[1, 1] - m[2, 2]))) / 2,
+			y = Mathf.Sqrt(Mathf.Max(0, (float)(1 - m[0, 0] + m[1, 1] - m[2, 2]))) / 2,
+			z = Mathf.Sqrt(Mathf.Max(0, (float)(1 - m[0, 0] - m[1, 1] + m[2, 2]))) / 2
+		};
+		q.x *= Mathf.Sign((float)(q.x * (m[2, 1] - m[1, 2])));
+		q.y *= Mathf.Sign((float)(q.y * (m[0, 2] - m[2, 0])));
+		q.z *= Mathf.Sign((float)(q.z * (m[1, 0] - m[0, 1])));
+		return q;
+	}
+
+	public double[,] LHMatrixFromRHMatrix(double[,] rhm)
+	{
+		double[,] lhm = new double[3,3] ;
+
+		// Column 0.
+		lhm[0, 0] = rhm[0, 0];
+		lhm[1, 0] = rhm[1, 0];
+		lhm[2, 0] = -rhm[2, 0];
+
+		// Column 1.
+		lhm[0, 1] = rhm[0, 1];
+		lhm[1, 1] = rhm[1, 1];
+		lhm[2, 1] = -rhm[2, 1];
+
+		// Column 2.
+		lhm[0, 2] = -rhm[0, 2];
+		lhm[1, 2] = -rhm[1, 2];
+		lhm[2, 2] = rhm[2, 2];
+
+		return lhm;
+	}
 }
---
+++ Assets/Scripts/TransformSmoother.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TransformSmoother : MonoBehaviour
+{
+    [Range(0,1)] [SerializeField]
+    private float smootherCoefPos=1;
+
+    [Range(0, 1)]
+    [SerializeField]
+    private float smootherCoefRot = 1;
+
+    public void SetNewTransform(Vector3 pos, Quaternion rot)
+    {
+        transform.position = Vector3.Lerp(transform.position, pos, smootherCoefPos);
+        transform.rotation = Quaternion.Slerp(transform.rotation, rot, smootherCoefRot);
+    }
+}
