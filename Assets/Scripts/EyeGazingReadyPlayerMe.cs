b4069ea7cd157dcf35348361fdda127576d5efa8
Crenau_Stagiaires Lucas_Detto <ldetto@enseignement.nantes.archi.fr>
2022-09-27 13:35:59 +0200

    Added basic eye gaze

---
+++ Assets/Scripts/EyeGazingReadyPlayerMe.cs
@@ -0,0 +1,115 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class EyeGazingReadyPlayerMe : MonoBehaviour
+{
+
+    [SerializeField] private Transform objectToTrack;
+    private SkinnedMeshRenderer meshRenderer;
+    Mesh skinnedMesh;
+    Transform leftEye;
+    Transform rightEye;
+
+    int eyeLookDownLeft;
+    int eyeLookInLeft;
+    int eyeLookOutLeft;
+    int eyeLookUpLeft;
+
+    int eyeLookDownRight;
+    int eyeLookInRight;
+    int eyeLookOutRight;
+    int eyeLookUpRight;
+
+    int eyesClosed;
+
+    float timeToBlink = 0.05f;
+    float timeBetweenBlinks = 3.5f;
+    float timeUntilNextBlink = 0;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        meshRenderer = GetComponent<SkinnedMeshRenderer>();
+        skinnedMesh = meshRenderer.sharedMesh;
+        leftEye = transform.GetChild(0);
+        rightEye = transform.GetChild(1);
+        eyeLookDownLeft = skinnedMesh.GetBlendShapeIndex("eyeLookDownLeft");
+        eyeLookInLeft = skinnedMesh.GetBlendShapeIndex("eyeLookInLeft");
+        eyeLookOutLeft = skinnedMesh.GetBlendShapeIndex("eyeLookOutLeft");
+        eyeLookUpLeft = skinnedMesh.GetBlendShapeIndex("eyeLookUpLeft");
+
+        eyeLookDownRight = skinnedMesh.GetBlendShapeIndex("eyeLookDownRight");
+        eyeLookInRight = skinnedMesh.GetBlendShapeIndex("eyeLookInRight");
+        eyeLookOutRight = skinnedMesh.GetBlendShapeIndex("eyeLookOutRight");
+        eyeLookUpRight = skinnedMesh.GetBlendShapeIndex("eyeLookUpRight");
+
+        eyesClosed = skinnedMesh.GetBlendShapeIndex("eyesClosed");
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        Vector3 directionLeft = leftEye.InverseTransformPoint(objectToTrack.transform.position).normalized*100;
+        if (directionLeft.z< 0)
+        {
+            meshRenderer.SetBlendShapeWeight(eyeLookDownLeft, Mathf.Clamp(-directionLeft.y, 0, 100));
+            meshRenderer.SetBlendShapeWeight(eyeLookUpLeft,Mathf.Clamp(directionLeft.y, 0, 100));
+            meshRenderer.SetBlendShapeWeight(eyeLookInLeft,Mathf.Clamp(-directionLeft.x, 0, 100));
+            meshRenderer.SetBlendShapeWeight(eyeLookOutLeft,Mathf.Clamp(directionLeft.x, 0, 100));
+        }
+        else
+        {
+            meshRenderer.SetBlendShapeWeight(eyeLookDownLeft, 0);
+            meshRenderer.SetBlendShapeWeight(eyeLookUpLeft, 0);
+            meshRenderer.SetBlendShapeWeight(eyeLookInLeft,0);
+            meshRenderer.SetBlendShapeWeight(eyeLookOutLeft, 0);
+        }
+        Vector3 directionRight = rightEye.InverseTransformPoint(objectToTrack.transform.position).normalized * 100;
+        if (directionRight.z< 0)
+        {
+            meshRenderer.SetBlendShapeWeight(eyeLookDownRight, Mathf.Clamp(-directionRight.y, 0, 100));
+            meshRenderer.SetBlendShapeWeight(eyeLookUpRight, Mathf.Clamp(directionRight.y, 0, 100));
+            meshRenderer.SetBlendShapeWeight(eyeLookInRight,Mathf.Clamp(directionRight.x, 0, 100));
+            meshRenderer.SetBlendShapeWeight(eyeLookOutRight,Mathf.Clamp(-directionRight.x, 0, 100));
+        }
+        else
+        {
+            meshRenderer.SetBlendShapeWeight(eyeLookDownRight, 0);
+            meshRenderer.SetBlendShapeWeight(eyeLookUpRight, 0);
+            meshRenderer.SetBlendShapeWeight(eyeLookInRight, 0);
+            meshRenderer.SetBlendShapeWeight(eyeLookOutRight, 0);
+        }
+
+        timeUntilNextBlink += Time.deltaTime;
+        if (timeUntilNextBlink > timeBetweenBlinks)
+        {
+            timeUntilNextBlink = 0;
+            StartCoroutine(nameof(BlinkingCoroutine));
+        }
+
+    }
+
+    IEnumerator BlinkingCoroutine()
+    {
+        float t = 0;
+        while (t < timeToBlink)
+        {
+            t += Time.deltaTime;
+            meshRenderer.SetBlendShapeWeight(eyesClosed, t*100/timeToBlink);
+            yield return new WaitForFixedUpdate();
+        }
+        t = timeToBlink;
+        while (t > 0)
+        {
+            t -= Time.deltaTime;
+            meshRenderer.SetBlendShapeWeight(eyesClosed, t * 100 / timeToBlink);
+            yield return new WaitForFixedUpdate();
+        }
+    }
+}
---
+++ Assets/Scripts/EyeGazingRocketbox.cs
@@ -0,0 +1,112 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EyeGazingRocketbox : MonoBehaviour
+{
+    [SerializeField] private Transform objectToTrack;
+    private SkinnedMeshRenderer meshRenderer;
+    Mesh skinnedMesh;
+    [SerializeField] Transform leftEye;
+    [SerializeField] Transform rightEye;
+
+    int eyeLookDownLeft;
+    int eyeLookInLeft;
+    int eyeLookOutLeft;
+    int eyeLookUpLeft;
+
+    int eyeLookDownRight;
+    int eyeLookInRight;
+    int eyeLookOutRight;
+    int eyeLookUpRight;
+
+    int eyesClosed;
+
+    float timeToBlink = 0.05f;
+    float timeBetweenBlinks = 3.5f;
+    float timeUntilNextBlink = 0;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        meshRenderer = GetComponentInChildren<SkinnedMeshRenderer>();
+        skinnedMesh = meshRenderer.sharedMesh;
+        eyeLookDownLeft = skinnedMesh.GetBlendShapeIndex("AK_11_EyeLookDownLeft");
+        eyeLookInLeft = skinnedMesh.GetBlendShapeIndex("AK_13_EyeLookInLeft");
+        eyeLookOutLeft = skinnedMesh.GetBlendShapeIndex("AK_15_EyeLookOutLeft");
+        eyeLookUpLeft = skinnedMesh.GetBlendShapeIndex("AK_17_EyeLookUpLeft");
+
+        eyeLookDownRight = skinnedMesh.GetBlendShapeIndex("AK_12_EyeLookDownRight");
+        eyeLookInRight = skinnedMesh.GetBlendShapeIndex("AK_14_EyeLookInRight");
+        eyeLookOutRight = skinnedMesh.GetBlendShapeIndex("AK_16_EyeLookOutRight");
+        eyeLookUpRight = skinnedMesh.GetBlendShapeIndex("AK_18_EyeLookUpRight");
+
+        eyesClosed = skinnedMesh.GetBlendShapeIndex("AU_45_Blink");
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        Vector3 directionLeft = leftEye.InverseTransformPoint(objectToTrack.transform.position).normalized * 100;
+        directionLeft = -directionLeft;
+        if (directionLeft.y < 0)
+        {
+            meshRenderer.SetBlendShapeWeight(eyeLookDownLeft, Mathf.Clamp(-directionLeft.z, 0, 100));
+            meshRenderer.SetBlendShapeWeight(eyeLookUpLeft, Mathf.Clamp(directionLeft.z, 0, 100));
+            meshRenderer.SetBlendShapeWeight(eyeLookInLeft, Mathf.Clamp(-directionLeft.x, 0, 100));
+            meshRenderer.SetBlendShapeWeight(eyeLookOutLeft, Mathf.Clamp(directionLeft.x, 0, 100));
+        }
+        else
+        {
+            meshRenderer.SetBlendShapeWeight(eyeLookDownLeft, 0);
+            meshRenderer.SetBlendShapeWeight(eyeLookUpLeft, 0);
+            meshRenderer.SetBlendShapeWeight(eyeLookInLeft, 0);
+            meshRenderer.SetBlendShapeWeight(eyeLookOutLeft, 0);
+        }
+        Vector3 directionRight = rightEye.InverseTransformPoint(objectToTrack.transform.position).normalized * 100;
+        directionRight = -directionRight;
+        if (directionRight.y < 0)
+        {
+            meshRenderer.SetBlendShapeWeight(eyeLookDownRight, Mathf.Clamp(-directionRight.z, 0, 100));
+            meshRenderer.SetBlendShapeWeight(eyeLookUpRight, Mathf.Clamp(directionRight.z, 0, 100));
+            meshRenderer.SetBlendShapeWeight(eyeLookInRight, Mathf.Clamp(directionRight.x, 0, 100));
+            meshRenderer.SetBlendShapeWeight(eyeLookOutRight, Mathf.Clamp(-directionRight.x, 0, 100));
+        }
+        else
+        {
+            meshRenderer.SetBlendShapeWeight(eyeLookDownRight, 0);
+            meshRenderer.SetBlendShapeWeight(eyeLookUpRight, 0);
+            meshRenderer.SetBlendShapeWeight(eyeLookInRight, 0);
+            meshRenderer.SetBlendShapeWeight(eyeLookOutRight, 0);
+        }
+
+        timeUntilNextBlink += Time.deltaTime;
+        if (timeUntilNextBlink > timeBetweenBlinks)
+        {
+            timeUntilNextBlink = 0;
+            StartCoroutine(nameof(BlinkingCoroutine));
+        }
+
+    }
+
+    IEnumerator BlinkingCoroutine()
+    {
+        float t = 0;
+        while (t < timeToBlink)
+        {
+            t += Time.deltaTime;
+            meshRenderer.SetBlendShapeWeight(eyesClosed, t * 100 / timeToBlink);
+            yield return new WaitForFixedUpdate();
+        }
+        t = timeToBlink;
+        while (t > 0)
+        {
+            t -= Time.deltaTime;
+            meshRenderer.SetBlendShapeWeight(eyesClosed, t * 100 / timeToBlink);
+            yield return new WaitForFixedUpdate();
+        }
+    }
+}
