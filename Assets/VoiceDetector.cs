51d5f35508516f607f7855ab732fda3e36bf1208
Crenau_Stagiaires Lucas_Detto <ldetto@enseignement.nantes.archi.fr>
2022-09-29 12:07:22 +0200

    Added basic voice detection scripts

---
+++ Assets/VoiceDetector.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VoiceDetector : MonoBehaviour
+{
+    bool isTalking = false;
+    static UnityEngine.Events.UnityEvent startedTalking;
+    static UnityEngine.Events.UnityEvent stoppedTalking;
+
+    [SerializeField] private float loudnessThreshold = 10e-4f;
+
+    private void Start()
+    {
+        startedTalking = new UnityEngine.Events.UnityEvent();
+        stoppedTalking = new UnityEngine.Events.UnityEvent();
+
+
+        startedTalking.AddListener(DebugStartTalking);
+        stoppedTalking.AddListener(DebugStopTalking);
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (MicrophoneDetector.MicLoudness > loudnessThreshold)
+        {
+            if (!isTalking)
+            {
+                isTalking = true;
+                startedTalking.Invoke();
+            }
+        }
+        else
+        {
+            if (isTalking)
+            {
+                isTalking = false;
+                stoppedTalking.Invoke();
+            }
+        }
+    }
+
+    void DebugStartTalking()
+    {
+        Debug.Log("Started talking");
+    }
+
+    void DebugStopTalking()
+    {
+        Debug.Log("stopped talking");
+    }
+}
