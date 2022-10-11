4d5ec223d287118286a1cfeb17560e17fb4e7d3e
Crenau_Stagiaires Lucas_Detto <ldetto@enseignement.nantes.archi.fr>
2022-10-11 09:36:46 +0200

    Added way to see lip camera video

---
+++ Assets/Scripts/LipCameraShow.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using ViveSR.anipal.Lip;
+
+public class LipCameraShow : MonoBehaviour
+{
+    Texture2D lipTexture;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        lipTexture = new Texture2D(SRanipal_Lip_v2.ImageWidth,SRanipal_Lip_v2.ImageHeight,TextureFormat.R8,false);
+        GetComponent<Renderer>().material.mainTexture = lipTexture;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        SRanipal_Lip_v2.GetLipImage(ref lipTexture);
+    }
+}
