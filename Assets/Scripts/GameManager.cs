94b3147a61aec8cb17d9346affadca14c341a7d1
Crenau_Stagiaires Lucas_Detto <ldetto@enseignement.nantes.archi.fr>
2022-10-17 15:20:15 +0200

    Added starting scene and GameManager

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+
+    public static GameManager Instance;
+
+    public bool HeadsActive = false;
+    public bool isCartoon = false;
+    public bool isMale = false;
+    public int participantID = -1;
+    public bool allowSceneChange = false;
+
+    void Start()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(this);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void ChangeScene(string SceneToLoad)
+    {
+        if (allowSceneChange)
+        {
+            UnityEngine.SceneManagement.SceneManager.LoadScene(SceneToLoad);
+        }
+    }
+
+    public void ChangeParticipantID(string id)
+    {
+        try
+        {
+            participantID = int.Parse(id);
+            allowSceneChange = true;
+        }
+        catch
+        {
+            allowSceneChange = false;
+        }
+    }
+
+    public void ToggleHeads()
+    {
+        HeadsActive = !HeadsActive;
+    }
+    public void ToggleCartoon()
+    {
+        isCartoon = !isCartoon;
+    }
+    public void ToggleGender()
+    {
+        isMale = !isMale;
+    }
+
+}
