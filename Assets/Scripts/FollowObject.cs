9def582c8391bdd0ed4bd02ae536594d17908ac7
Crenau_Stagiaires Lucas_Detto <ldetto@enseignement.nantes.archi.fr>
2022-10-27 15:57:56 +0200

    Update FollowObject.cs

--- Assets/Scripts/FollowObject.cs
+++ Assets/Scripts/FollowObject.cs
@@ -8,16 +8,11 @@ public class FollowObject : NetworkBehaviour
 
     [SerializeField] Transform objectToFollow;
 
-    private void Start()
-    {
-        objectToFollow = GameObject.FindGameObjectWithTag("Player").transform.GetChild(0).GetChild(0);
-    }
-
     private void Update()
     {
         if (netIdentity.isLocalPlayer&& objectToFollow == null)
         {
-            Start();
+            objectToFollow = GameObject.FindGameObjectWithTag("Player").transform.GetChild(0).GetChild(0);
         }
         transform.position = objectToFollow.position;
         transform.rotation = objectToFollow.rotation;
