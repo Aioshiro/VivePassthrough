cea712670f613ec3e83ba60788e2661b55f2a27e
Crenau_Stagiaires Lucas_Detto <ldetto@enseignement.nantes.archi.fr>
2022-10-27 15:49:22 +0200

    updated

---
+++ Assets/Scripts/FollowObject.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+
+public class FollowObject : NetworkBehaviour
+{
+
+    [SerializeField] Transform objectToFollow;
+
+    private void Start()
+    {
+        objectToFollow = GameObject.FindGameObjectWithTag("Player").transform.GetChild(0).GetChild(0);
+    }
+
+    private void Update()
+    {
+        if (netIdentity.isLocalPlayer&& objectToFollow == null)
+        {
+            Start();
+        }
+        transform.position = objectToFollow.position;
+        transform.rotation = objectToFollow.rotation;
+    }
+}
