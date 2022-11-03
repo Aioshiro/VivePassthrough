8f6ac42be2a8f544fc0c478af34d80cd7bc43e4c
Crenau_Stagiaires Lucas_Detto <ldetto@enseignement.nantes.archi.fr>
2022-11-03 10:29:42 +0100

    syncheadrotation

--- Assets/Scripts/SyncHeads.cs
+++ Assets/Scripts/SyncHeads.cs
@@ -6,21 +6,25 @@ using Mirror;
 public class SyncHeads : NetworkBehaviour
 {
     readonly SyncList<Vector3> playersHeadsLocalPositions = new SyncList<Vector3>();
+    readonly SyncList<Quaternion> playersHeadsLocalRotations = new SyncList<Quaternion>();
     [SerializeField] List<GameObject> playersHeads;
     [SerializeField] Transform markerWorldOrigin;
-    [SerializeField] Transform localRigTrackedLeftCamera;
+    [SerializeField] Transform localRigTrackedCamera;
 
     public override void OnStartServer()
     {
         base.OnStartServer();
         playersHeadsLocalPositions.Add(Vector3.zero);
         playersHeadsLocalPositions.Add(Vector3.zero);
+        playersHeadsLocalRotations.Add(Quaternion.identity);
+        playersHeadsLocalRotations.Add(Quaternion.identity);
     }
 
     public override void OnStartClient()
     {
         base.OnStartClient();
-        playersHeadsLocalPositions.Callback += OnListUpdated;
+        playersHeadsLocalPositions.Callback += OnPosUpdated;
+        playersHeadsLocalRotations.Callback += OnRotUpdated;
     }
 
     // Update is called once per frame
@@ -29,20 +33,28 @@ public class SyncHeads : NetworkBehaviour
         if (isClientOnly)
         {
             int playerNumber = GameManager.Instance.playerNumber;
-            //First, upload own headPos
-            UpdateListValue(playerNumber, markerWorldOrigin.transform.InverseTransformPoint(localRigTrackedLeftCamera.position));
-            //Then, download other headPos
+            //First, upload own headPos and headRot
+            UpdatePosValue(playerNumber, markerWorldOrigin.transform.InverseTransformPoint(localRigTrackedCamera.position));
+            UpdateRotValue(playerNumber, localRigTrackedCamera.transform.rotation * markerWorldOrigin.transform.rotation);
+            //Then, download other headPos and headRot
             playersHeads[(playerNumber + 1) % 2].transform.position = markerWorldOrigin.transform.TransformPoint(playersHeadsLocalPositions[(playerNumber + 1) % 2]);
+            playersHeads[(playerNumber + 1) % 2].transform.rotation = markerWorldOrigin.transform.rotation*playersHeadsLocalRotations[(playerNumber + 1) % 2];
         }
     }
 
     [Command(requiresAuthority =false)]
-    void UpdateListValue(int index, Vector3 newValue)
+    void UpdatePosValue(int index, Vector3 newValue)
     {
         playersHeadsLocalPositions[index] = newValue;
     }
 
-    void OnListUpdated(SyncList<Vector3>.Operation op, int index, Vector3 oldItem, Vector3 newItem)
+    [Command(requiresAuthority = false)]
+    void UpdateRotValue(int index, Quaternion newValue)
+    {
+        playersHeadsLocalRotations[index] = newValue;
+    }
+
+    void OnPosUpdated(SyncList<Vector3>.Operation op, int index, Vector3 oldItem, Vector3 newItem)
     {
         switch (op)
         {
@@ -68,5 +80,32 @@ public class SyncHeads : NetworkBehaviour
                 break;
         }
     }
-    
+
+    void OnRotUpdated(SyncList<Quaternion>.Operation op, int index, Quaternion oldItem, Quaternion newItem)
+    {
+        switch (op)
+        {
+            case SyncList<Quaternion>.Operation.OP_ADD:
+                // index is where it was added into the list
+                // newItem is the new item
+                break;
+            case SyncList<Quaternion>.Operation.OP_INSERT:
+                // index is where it was inserted into the list
+                // newItem is the new item
+                break;
+            case SyncList<Quaternion>.Operation.OP_REMOVEAT:
+                // index is where it was removed from the list
+                // oldItem is the item that was removed
+                break;
+            case SyncList<Quaternion>.Operation.OP_SET:
+                // index is of the item that was changed
+                // oldItem is the previous value for the item at the index
+                // newItem is the new value for the item at the index
+                break;
+            case SyncList<Quaternion>.Operation.OP_CLEAR:
+                // list got cleared
+                break;
+        }
+    }
+
 }
