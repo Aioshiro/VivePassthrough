c208a1737d1c5acf0e1318c1f28897a203de9b1f
Crenau_Stagiaires Lucas_Detto <ldetto@enseignement.nantes.archi.fr>
2022-11-09 13:35:38 +0100

    Added sphere hands

---
+++ Assets/Scripts/SphereOccluder.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using ViveHandTracking;
+
+public class SphereOccluder : MonoBehaviour
+{
+    [Tooltip("Draw left hand if true, right hand otherwise")]
+    public bool IsLeft = false;
+    [Tooltip("Root object of skinned mesh")]
+    public GameObject Hand = null;
+
+
+    void Awake()
+    {
+        Hand.SetActive(false);
+    }
+
+    IEnumerator Start()
+    {
+        while (GestureProvider.Status == GestureStatus.NotStarted) yield return null;
+        if (!GestureProvider.HaveSkeleton)
+        {
+            this.enabled = false;
+        }
+    }
+
+    void Update()
+    {
+        GestureResult result = IsLeft ? GestureProvider.LeftHand : GestureProvider.RightHand;
+        if (result == null)
+        {
+            Hand.SetActive(false);
+            return;
+        }
+        Hand.SetActive(true);
+
+        transform.localPosition = result.position;
+        transform.localRotation = result.rotation;
+    }
+}
+
