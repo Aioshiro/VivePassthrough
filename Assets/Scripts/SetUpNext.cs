a19cd73280f76a7401cdd56155c11d76c0d13756
Crenau_Stagiaires Lucas_Detto <ldetto@enseignement.nantes.archi.fr>
2022-11-25 10:31:13 +0100

    Added Mirror script

---
+++ Assets/Scripts/SetUpNext.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using ViveSR.anipal.Eye;
+
+public class SetUpNext : MonoBehaviour
+{
+
+    private const float Distance = 0.6f;
+
+    private void Update()
+    {
+        if (SRanipal_Eye_Framework.Status != SRanipal_Eye_Framework.FrameworkStatus.WORKING) return;
+        SetMirroTransform();
+    }
+
+    private void SetMirroTransform()
+    {
+        transform.position = Camera.main.transform.position + Camera.main.transform.forward * Distance;
+        transform.position = new Vector3(transform.position.x, Camera.main.transform.position.y, transform.position.z);
+        transform.LookAt(Camera.main.transform);
+        transform.eulerAngles = new Vector3(0, transform.eulerAngles.y, 0);
+    }
+}
