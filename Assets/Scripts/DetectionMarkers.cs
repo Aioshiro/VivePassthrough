7eb6a4526baa6b6413c352c246f6ac1a45671a45
Crenau_Stagiaires Lucas_Detto <ldetto@enseignement.nantes.archi.fr>
2022-12-01 14:49:01 +0100

    Clean up detection marker code right camera

--- Assets/Scripts/DetectionMarkers.cs
+++ Assets/Scripts/DetectionMarkers.cs
@@ -13,8 +13,8 @@ public class DetectionMarkers : MonoBehaviour
 	private Dictionary dictionary;
 	Texture2D left;
 	Texture2D leftCPU;
-	Texture2D right;
-	Texture2D rightCPU;
+	//Texture2D right;
+	//Texture2D rightCPU;
 	[Tooltip("Check if you want to do AruCo corner refinement (advised)")]
 	public bool useCornerRefinement;
 	Point3f[][] markerPoints; //Marker points in object space
@@ -25,18 +25,18 @@ public class DetectionMarkers : MonoBehaviour
 	[SerializeField] int numberOfMarkers = 10;
 	MarkersManager markersManager; //Manager for markers position and smoothering
 	//MarkersManagerMulti markersManager;
-	bool[] markersToUpdateRight;
+	//bool[] markersToUpdateRight;
 	bool[] markersToUpdateLeft;
 
 	//Aruco results
 	private Point2f[][] corners;
 	private int[] ids;
 	double[][,] rotMatLeft;
-	double[][,] rotMatRight;
+	//double[][,] rotMatRight;
 	double[][] rvecLeft;
-	private double[][] rvecRight;
+	//private double[][] rvecRight;
 	double[][] tvecLeft;
-	private double[][] tvecRight;
+	//private double[][] tvecRight;
 
 	//To extract the HMD video frame from GPU to CPU
 	private bool initialized=false;
@@ -47,16 +47,16 @@ public class DetectionMarkers : MonoBehaviour
 
 	//Camera parameters
 	private double[,] cameraLeftMatrix;
-	private double[,] cameraRightMatrix; //camera intrinsic parameters
+	//private double[,] cameraRightMatrix; //camera intrinsic parameters
     readonly double[] distCoeffs = new double[4] { 0d, 0d, 0d, 0d }; //no distortion with the Vive Pro 2
-	private bool isCameraRightInitialized = false;
+	//private bool isCameraRightInitialized = false;
 	private bool isCameraLeftInitialized = false;
-	[Tooltip("Right camera transform")]
-	[SerializeField] Transform cameraRight;
+	//[Tooltip("Right camera transform")]
+	//[SerializeField] Transform cameraRight;
 	[Tooltip("Left camera transform")]
 	[SerializeField] Transform cameraLeft;
 	public bool useLeftCamera;
-	public bool useRightCamera;
+	//public bool useRightCamera;
 
 	OpenCvSharp.Unity.TextureConversionParams conversionParams;
 
@@ -67,7 +67,7 @@ public class DetectionMarkers : MonoBehaviour
 		//markersManager = GetComponent<MarkersManagerMulti>();
 		markersManager = GetComponent<MarkersManager>();
 		markersToUpdateLeft = new bool[numberOfMarkers];
-		markersToUpdateRight = new bool[numberOfMarkers];
+		//markersToUpdateRight = new bool[numberOfMarkers];
 		InitArucoParameters();
 	}
 	/// <summary>
@@ -117,72 +117,72 @@ public class DetectionMarkers : MonoBehaviour
 
 		rotMatLeft = new double[numberOfMarkers][,];
 
-		rvecRight = new double[numberOfMarkers][];
-		tvecRight = new double[numberOfMarkers][];
+		//rvecRight = new double[numberOfMarkers][];
+		//tvecRight = new double[numberOfMarkers][];
 
-		rotMatRight = new double[numberOfMarkers][,];
+		//rotMatRight = new double[numberOfMarkers][,];
 	}
 
-	public void IncreaseWindow(UnityEngine.InputSystem.InputAction.CallbackContext context)
-    {
-		if (context.started)
-		{
-			detectorParameters.CornerRefinementWinSize += 1;
-			Debug.Log("Window size is " + detectorParameters.CornerRefinementWinSize.ToString());
-		}
-    }
-	public void DecreaseWindow(UnityEngine.InputSystem.InputAction.CallbackContext context)
-    {
-		if (context.started)
-		{
-			detectorParameters.CornerRefinementWinSize -= 1;
-			if (detectorParameters.CornerRefinementWinSize <= 0)
-			{
-				detectorParameters.CornerRefinementWinSize = 1;
-			}
-			Debug.Log("Window size is " + detectorParameters.CornerRefinementWinSize.ToString());
-		}
-	}
+	//public void IncreaseWindow(UnityEngine.InputSystem.InputAction.CallbackContext context)
+ //   {
+	//	if (context.started)
+	//	{
+	//		detectorParameters.CornerRefinementWinSize += 1;
+	//		Debug.Log("Window size is " + detectorParameters.CornerRefinementWinSize.ToString());
+	//	}
+ //   }
+	//public void DecreaseWindow(UnityEngine.InputSystem.InputAction.CallbackContext context)
+ //   {
+	//	if (context.started)
+	//	{
+	//		detectorParameters.CornerRefinementWinSize -= 1;
+	//		if (detectorParameters.CornerRefinementWinSize <= 0)
+	//		{
+	//			detectorParameters.CornerRefinementWinSize = 1;
+	//		}
+	//		Debug.Log("Window size is " + detectorParameters.CornerRefinementWinSize.ToString());
+	//	}
+	//}
 
-	public void IncreaseCornerMinAccuracy(UnityEngine.InputSystem.InputAction.CallbackContext context)
-	{
-		if (context.started)
-        {
-			detectorParameters.CornerRefinementMinAccuracy *= 10;
-			Debug.Log("Min accuracy is " + detectorParameters.CornerRefinementMinAccuracy.ToString());
-        }
-	}
+	//public void IncreaseCornerMinAccuracy(UnityEngine.InputSystem.InputAction.CallbackContext context)
+	//{
+	//	if (context.started)
+ //       {
+	//		detectorParameters.CornerRefinementMinAccuracy *= 10;
+	//		Debug.Log("Min accuracy is " + detectorParameters.CornerRefinementMinAccuracy.ToString());
+ //       }
+	//}
 
-	public void DecreaseCornerMinAccuracy(UnityEngine.InputSystem.InputAction.CallbackContext context)
-	{
-		if (context.started)
-		{
-			detectorParameters.CornerRefinementMinAccuracy /= 10;
-			Debug.Log("Min accuracy is " + detectorParameters.CornerRefinementMinAccuracy.ToString());
-		}
-	}
+	//public void DecreaseCornerMinAccuracy(UnityEngine.InputSystem.InputAction.CallbackContext context)
+	//{
+	//	if (context.started)
+	//	{
+	//		detectorParameters.CornerRefinementMinAccuracy /= 10;
+	//		Debug.Log("Min accuracy is " + detectorParameters.CornerRefinementMinAccuracy.ToString());
+	//	}
+	//}
 
-	public void DecreasePolygonalApproxAccuracyRate(UnityEngine.InputSystem.InputAction.CallbackContext context)
-	{
-		if (context.started)
-		{
-			detectorParameters.PolygonalApproxAccuracyRate -= 0.01;
-			if (detectorParameters.PolygonalApproxAccuracyRate <= 0)
-            {
-				detectorParameters.PolygonalApproxAccuracyRate = 0.01;
-            }
-			Debug.Log("Adaptive PolygonalApproxAccuracyRate is " + detectorParameters.PolygonalApproxAccuracyRate.ToString());
-		}
-	}
+	//public void DecreasePolygonalApproxAccuracyRate(UnityEngine.InputSystem.InputAction.CallbackContext context)
+	//{
+	//	if (context.started)
+	//	{
+	//		detectorParameters.PolygonalApproxAccuracyRate -= 0.01;
+	//		if (detectorParameters.PolygonalApproxAccuracyRate <= 0)
+ //           {
+	//			detectorParameters.PolygonalApproxAccuracyRate = 0.01;
+ //           }
+	//		Debug.Log("Adaptive PolygonalApproxAccuracyRate is " + detectorParameters.PolygonalApproxAccuracyRate.ToString());
+	//	}
+	//}
 
-	public void IncreasePolygonalApproxAccuracyRate(UnityEngine.InputSystem.InputAction.CallbackContext context)
-	{
-		if (context.started)
-		{
-			detectorParameters.PolygonalApproxAccuracyRate += 0.01;
-			Debug.Log("Adaptive PolygonalApproxAccuracyRate is " + detectorParameters.PolygonalApproxAccuracyRate.ToString());
-		}
-	}
+	//public void IncreasePolygonalApproxAccuracyRate(UnityEngine.InputSystem.InputAction.CallbackContext context)
+	//{
+	//	if (context.started)
+	//	{
+	//		detectorParameters.PolygonalApproxAccuracyRate += 0.01;
+	//		Debug.Log("Adaptive PolygonalApproxAccuracyRate is " + detectorParameters.PolygonalApproxAccuracyRate.ToString());
+	//	}
+	//}
 
 
 	/// <summary>
@@ -192,8 +192,8 @@ public class DetectionMarkers : MonoBehaviour
 	{
 		left = new Texture2D(ViveSR_DualCameraImageCapture.UndistortedImageWidth, ViveSR_DualCameraImageCapture.UndistortedImageHeight, TextureFormat.RGBA32, false);
 		leftCPU = new Texture2D(ViveSR_DualCameraImageCapture.UndistortedImageWidth, ViveSR_DualCameraImageCapture.UndistortedImageHeight, TextureFormat.RGBA32, false);
-		right = new Texture2D(ViveSR_DualCameraImageCapture.UndistortedImageWidth, ViveSR_DualCameraImageCapture.UndistortedImageHeight, TextureFormat.RGBA32, false);
-		rightCPU = new Texture2D(ViveSR_DualCameraImageCapture.UndistortedImageWidth, ViveSR_DualCameraImageCapture.UndistortedImageHeight, TextureFormat.RGBA32, false);
+		//right = new Texture2D(ViveSR_DualCameraImageCapture.UndistortedImageWidth, ViveSR_DualCameraImageCapture.UndistortedImageHeight, TextureFormat.RGBA32, false);
+		//rightCPU = new Texture2D(ViveSR_DualCameraImageCapture.UndistortedImageWidth, ViveSR_DualCameraImageCapture.UndistortedImageHeight, TextureFormat.RGBA32, false);
 	}
 
 	/// <summary>
@@ -209,11 +209,11 @@ public class DetectionMarkers : MonoBehaviour
 	/// Called when right camera request is finished
 	/// </summary>
 	/// <param name="request"></param>
-	void OnCompleteReadbackRight(AsyncGPUReadbackRequest request)
-	{
-		//Loading data into texture
-		rightCPU.LoadRawTextureData(request.GetData<uint>());
-	}
+	//void OnCompleteReadbackRight(AsyncGPUReadbackRequest request)
+	//{
+	//	//Loading data into texture
+	//	rightCPU.LoadRawTextureData(request.GetData<uint>());
+	//}
 
 	/// <summary>
 	/// Initialize the left camera
@@ -237,21 +237,21 @@ public class DetectionMarkers : MonoBehaviour
 	/// <summary>
 	/// Initialize the right camera
 	/// </summary>
-	void InitRightCamera()
-	{
-		cameraRightMatrix = new double[3, 3] {
-				{ViveSR_DualCameraImageCapture.FocalLengthRight, 0d, ViveSR_DualCameraImageCapture.UndistortedCxRight},
-				{0d, ViveSR_DualCameraImageCapture.FocalLengthRight, ViveSR_DualCameraImageCapture.UndistortedCyRight},
-				{0d, 0d, 1d}
-			};
+	//void InitRightCamera()
+	//{
+	//	cameraRightMatrix = new double[3, 3] {
+	//			{ViveSR_DualCameraImageCapture.FocalLengthRight, 0d, ViveSR_DualCameraImageCapture.UndistortedCxRight},
+	//			{0d, ViveSR_DualCameraImageCapture.FocalLengthRight, ViveSR_DualCameraImageCapture.UndistortedCyRight},
+	//			{0d, 0d, 1d}
+	//		};
 
-		ViveSR_DualCameraImageCapture.GetUndistortedTexture(out _, out right, out _, out _, out _, out _);
-		if (right != null) //Sometimes, camera is not active on first frame
-		{
-			requestRight = AsyncGPUReadback.Request(right, 0, TextureFormat.RGBA32, OnCompleteReadbackRight);
-			isCameraRightInitialized = true;
-		}
-	}
+	//	ViveSR_DualCameraImageCapture.GetUndistortedTexture(out _, out right, out _, out _, out _, out _);
+	//	if (right != null) //Sometimes, camera is not active on first frame
+	//	{
+	//		requestRight = AsyncGPUReadback.Request(right, 0, TextureFormat.RGBA32, OnCompleteReadbackRight);
+	//		isCameraRightInitialized = true;
+	//	}
+	//}
 
 	private void Update()
 	{
@@ -262,10 +262,10 @@ public class DetectionMarkers : MonoBehaviour
 		}
 		if (!initialized) { return; }
 
-		if (!isCameraRightInitialized && ViveSR_DualCameraImageCapture.FocalLengthRight > 0) //Sometimes, capture is not initialized on first frame
-		{
-			InitRightCamera();
-		}
+		//if (!isCameraRightInitialized && ViveSR_DualCameraImageCapture.FocalLengthRight > 0) //Sometimes, capture is not initialized on first frame
+		//{
+		//	InitRightCamera();
+		//}
 
 		if (!isCameraLeftInitialized && ViveSR_DualCameraImageCapture.FocalLengthLeft > 0) //Sometimes, capture is not initialized on first frame
 		{
@@ -275,27 +275,27 @@ public class DetectionMarkers : MonoBehaviour
         {
 			cameraLeft = GameObject.FindGameObjectWithTag("Player").transform.GetChild(0).GetChild(0);
         }
-		if (cameraRight == null)
-        {
-			cameraRight = GameObject.FindGameObjectWithTag("Player").transform.GetChild(0).GetChild(1);
-		}
-		if (useRightCamera && isCameraRightInitialized&&requestRight.done) //if the camera is initialized and we got the gpu texture
-		{
-			DetectMarkers(rightCPU, out corners, out ids); //Detect every marker on the image
-			for (int i = 0; i < ids.Length; i++) //Updating position and rotation of every marker
-			{
-				if (ids[i] <numberOfMarkers)
-				{
-					markersToUpdateRight[ids[i]] = true;
-					Cv2.SolvePnP(markerPoints[ids[i]], corners[i], cameraRightMatrix, distCoeffs, out rvecRight[ids[i]], out tvecRight[ids[i]], false, SolvePnPFlags.Iterative); //Pose estimation, to go from 2d pixels to 3d position in CAMERA space
-					Cv2.Rodrigues(rvecRight[ids[i]], out rotMatRight[ids[i]]);
-				}
-			}
+		//if (cameraRight == null)
+  //      {
+		//	cameraRight = GameObject.FindGameObjectWithTag("Player").transform.GetChild(0).GetChild(1);
+		//}
+		//if (useRightCamera && isCameraRightInitialized&&requestRight.done) //if the camera is initialized and we got the gpu texture
+		//{
+		//	DetectMarkers(rightCPU, out corners, out ids); //Detect every marker on the image
+		//	for (int i = 0; i < ids.Length; i++) //Updating position and rotation of every marker
+		//	{
+		//		if (ids[i] <numberOfMarkers)
+		//		{
+		//			markersToUpdateRight[ids[i]] = true;
+		//			Cv2.SolvePnP(markerPoints[ids[i]], corners[i], cameraRightMatrix, distCoeffs, out rvecRight[ids[i]], out tvecRight[ids[i]], false, SolvePnPFlags.Iterative); //Pose estimation, to go from 2d pixels to 3d position in CAMERA space
+		//			Cv2.Rodrigues(rvecRight[ids[i]], out rotMatRight[ids[i]]);
+		//		}
+		//	}
 
-			ViveSR_DualCameraImageCapture.GetUndistortedTexture(out _, out right, out _, out _, out _, out _);
-			requestRight = AsyncGPUReadback.Request(right, 0, TextureFormat.RGBA32, OnCompleteReadbackRight);
-			if (waitForCompletion) { requestRight.WaitForCompletion(); }
-		}
+		//	ViveSR_DualCameraImageCapture.GetUndistortedTexture(out _, out right, out _, out _, out _, out _);
+		//	requestRight = AsyncGPUReadback.Request(right, 0, TextureFormat.RGBA32, OnCompleteReadbackRight);
+		//	if (waitForCompletion) { requestRight.WaitForCompletion(); }
+		//}
 
 		if (useLeftCamera && isCameraLeftInitialized && requestLeft.done) //if the camera is initialized and we got the gpu texture
 		{
@@ -325,29 +325,31 @@ public class DetectionMarkers : MonoBehaviour
 	{
 		for (int i = 0; i < markersToUpdateLeft.Length; i++)
 		{
-			if (markersToUpdateLeft[i] && markersToUpdateRight[i]) //foreach marker detected and so needed to be updated
+			//if (markersToUpdateLeft[i] && markersToUpdateRight[i]) //foreach marker detected and so needed to be updated
+			//{
+			//	markersToUpdateLeft[i] = false;
+			//	markersToUpdateRight[i] = false;
+			//	GetObjectNewTransform(tvecRight[i], rotMatRight[i], false, out Vector3 worldPosRight, out Quaternion worldRotRight); //estimating worldspace position
+			//	GetObjectNewTransform(tvecLeft[i], rotMatLeft[i],true, out Vector3 worldPosLeft, out Quaternion worldRotLeft); //estimating worldspace position
+			//	markersManager.UpdateIthMarkerPos(i, (worldPosLeft + worldPosRight) / 2, Quaternion.Slerp(worldRotRight,worldRotLeft,0.5f));
+			//	//markersManager.SetActiveIthMarker(i, true);
+			//}
+			// else if(markersToUpdateLeft[i])
+
+			if (markersToUpdateLeft[i])
 			{
 				markersToUpdateLeft[i] = false;
-				markersToUpdateRight[i] = false;
-				GetObjectNewTransform(tvecRight[i], rotMatRight[i], false, out Vector3 worldPosRight, out Quaternion worldRotRight); //estimating worldspace position
-				GetObjectNewTransform(tvecLeft[i], rotMatLeft[i],true, out Vector3 worldPosLeft, out Quaternion worldRotLeft); //estimating worldspace position
-				markersManager.UpdateIthMarkerPos(i, (worldPosLeft + worldPosRight) / 2, Quaternion.Slerp(worldRotRight,worldRotLeft,0.5f));
-				//markersManager.SetActiveIthMarker(i, true);
-			}
-            else if(markersToUpdateLeft[i])
-			{
-				markersToUpdateLeft[i] = false;
-				GetObjectNewTransform(tvecLeft[i],rotMatLeft[i],true, out Vector3 worldPos, out Quaternion worldRot); //estimating worldspace position
+				GetObjectNewTransform(tvecLeft[i], rotMatLeft[i], out Vector3 worldPos, out Quaternion worldRot); //estimating worldspace position
 				markersManager.UpdateIthMarkerPos(i, worldPos, worldRot);
 				//markersManager.SetActiveIthMarker(i, true);
 			}
-			else if (markersToUpdateRight[i])
-            {
-				markersToUpdateRight[i] = false;
-				GetObjectNewTransform(tvecRight[i], rotMatRight[i],false, out Vector3 worldPos, out Quaternion worldRot); //estimating worldspace position
-				markersManager.UpdateIthMarkerPos(i, worldPos, worldRot);
-				//markersManager.SetActiveIthMarker(i, true);
-			}
+			//else if (markersToUpdateRight[i])
+			//{
+			//	markersToUpdateRight[i] = false;
+			//	GetObjectNewTransform(tvecRight[i], rotMatRight[i], false, out Vector3 worldPos, out Quaternion worldRot); //estimating worldspace position
+			//	markersManager.UpdateIthMarkerPos(i, worldPos, worldRot);
+			//	//markersManager.SetActiveIthMarker(i, true);
+			//}
    //         else
    //         {
 			//	//markersManager.SetActiveIthMarker(i, false);
@@ -363,36 +365,20 @@ public class DetectionMarkers : MonoBehaviour
 	/// <param name="isLeft"> True if this is left camera, right otherwise</param>
 	/// <param name="worldPos"> Object position in world space</param>
 	/// <param name="worldRot"> Object rotation in world space</param>
-	private void GetObjectNewTransform(double[] tvec,double[,] rotMat, bool isLeft,out Vector3 worldPos, out Quaternion worldRot)
+	private void GetObjectNewTransform(double[] tvec,double[,] rotMat, out Vector3 worldPos, out Quaternion worldRot)
     {
 		worldPos = new Vector3(-(float)tvec[0], (float)tvec[1], (float)tvec[2]); //initial pos in cameraSpace,  x is negative due to flipping of camera image
 
 		//now going from camera space to worldspace
-		if (isLeft)
-		{
-			worldPos = cameraLeft.TransformPoint(worldPos); //straightforward transformation for position
-		}
-		else
-		{
-			worldPos = cameraRight.TransformPoint(worldPos); //straightforward transformation for position
-		}
-
+		worldPos = cameraLeft.TransformPoint(worldPos); //straightforward transformation for position
 		Quaternion cameraRot = QuaternionUtil.QuaternionFromMatrix(rotMat); //get the rotation in camera space
 
 		cameraRot.y = -cameraRot.y;   //have to invert y and z due to right hand convention in OpenCV
 		cameraRot.z = -cameraRot.z;   //and left hand convention in Unity
 
 		//Quaternion cameraPos;
-		if (isLeft)
-        {
-			worldRot = cameraLeft.rotation * cameraRot;
-			//cameraPos = cameraLeft.rotation; //get the camera rotation
-		}
-        else
-        {
-			worldRot = cameraRight.rotation * cameraRot;
-			//cameraPos = cameraRight.rotation; //get the camera rotation
-		}
+		worldRot = cameraLeft.rotation * cameraRot;
+		//cameraPos = cameraLeft.rotation; //get the camera rotation
 
 		//worldRot = cameraPos * cameraRot; //composition of rotations by multiplying quaternions
 
