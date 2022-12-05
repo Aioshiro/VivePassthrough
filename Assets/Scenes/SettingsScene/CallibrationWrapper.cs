3ade2d342571cce8143cc4da513a8bbdf714a0a8
Crenau_Stagiaires Lucas_Detto <ldetto@enseignement.nantes.archi.fr>
2022-12-05 15:37:39 +0100

    Added way to skip eye callibration

--- Assets/Scenes/SettingsScene/CallibrationWrapper.cs
+++ Assets/Scenes/SettingsScene/CallibrationWrapper.cs
@@ -4,16 +4,27 @@ using UnityEngine;
 using ViveSR.anipal.Eye;
 using System;
 
+
+
 /// <summary>
 /// Wrapper script to start eye callibration
 /// </summary>
 public class CallibrationWrapper : MonoBehaviour
 {
+
+    public bool skipCallibration = false;
+
+    public void ToggleSkipCallibration(bool skip)
+    {
+        skipCallibration = skip;
+    }
+
     /// <summary>
     /// Function to call to start eye callibration
     /// </summary>
     public void StartCallibration()
     {
+        if (skipCallibration) { return; }
         SRanipal_Eye_API.LaunchEyeCalibration(IntPtr.Zero);
     }
 }
