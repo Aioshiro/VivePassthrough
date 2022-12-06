b1d453fc406f5e7413e715ed70e98c43254b71ba
Crenau_Stagiaires Lucas_Detto <ldetto@enseignement.nantes.archi.fr>
2022-12-06 10:15:47 +0100

    Added canvases behind players

---
+++ Assets/Scenes/OnlineScene/ActivateRightCanvas.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActivateRightCanvas : MonoBehaviour
+{
+    [SerializeField]
+    Transform playerRig;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Vector3 cross = Vector3.Cross(transform.forward, playerRig.forward);
+        float dot = Vector3.Dot(cross, transform.up);
+        if (dot < 0)
+        {
+            transform.GetChild(1).gameObject.SetActive(true);
+        }
+        else
+        {
+            transform.GetChild(0).gameObject.SetActive(true);
+        }
+    }
+
+}
