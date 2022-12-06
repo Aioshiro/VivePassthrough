7f990afce8d72baee4691f743bf84be419657e46
Crenau_Stagiaires Lucas_Detto <ldetto@enseignement.nantes.archi.fr>
2022-12-06 14:43:11 +0100

    Syncing heads trhough base stations

--- Assets/Scripts/SyncHeads.cs
+++ Assets/Scripts/SyncHeads.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using Mirror;
+
 /// <summary>
 /// Sync the head positions and rotations of the players on the server.
 /// <para>It uploads the local position and rotation in the marker 10 space, so both client have same origin</para>
@@ -17,8 +18,12 @@ public class SyncHeads : NetworkBehaviour
     [Tooltip("Other player head")]
     [SerializeField] GameObject otherPlayerHead;
 
-    [Tooltip("Marker 10 transform")]
-    [SerializeField] TransformSmoother markerWorldOrigin;
+    //[Tooltip("Marker 10 transform")]
+    //[SerializeField] TransformSmoother markerWorldOrigin;
+
+    private GameObject baseStationOrigin;
+    Valve.VR.TrackedDevicePose_t[] stationsPose = new Valve.VR.TrackedDevicePose_t[3];
+
 
     [Tooltip("Local rig tracked camera")]
     [SerializeField] Transform localRigTrackedCamera;
@@ -50,6 +55,7 @@ public class SyncHeads : NetworkBehaviour
         base.OnStartClient();
         playersHeadsLocalPositions.Callback += OnPosUpdated;
         playersHeadsLocalRotations.Callback += OnRotUpdated;
+        baseStationOrigin = new GameObject("Base station");
     }
 
     // Update is called once per frame
@@ -62,20 +68,41 @@ public class SyncHeads : NetworkBehaviour
         if (isClientOnly)
         {
             int playerNumber = GameManager.Instance.playerNumber;
-            //First, upload own headPos and headRot
-            UpdatePosValue(playerNumber, markerWorldOrigin.transform.InverseTransformPoint(localRigTrackedCamera.position));
-            UpdateRotValue(playerNumber, Quaternion.Inverse(markerWorldOrigin.transform.rotation)* localRigTrackedCamera.transform.rotation);
-            //Then, download other headPos and headRot
-            otherPlayerHead.transform.SetPositionAndRotation(markerWorldOrigin.transform.TransformPoint(playersHeadsLocalPositions[(playerNumber + 1) % 2]), markerWorldOrigin.transform.rotation*playersHeadsLocalRotations[(playerNumber + 1) % 2]);
-            if (!hasSentMarkerTransform && markerWorldOrigin.count == markerWorldOrigin.movingAverageLengthPos)
-            {
-                SendMarkerPosToServer(playerNumber, markerWorldOrigin.transform.position);
-                hasSentMarkerTransform = true;
-                Debug.Log("Sending marker pos to server");
-            }
-        }
-    }
 
+            Valve.VR.OpenVR.System.GetDeviceToAbsoluteTrackingPose(Valve.VR.ETrackingUniverseOrigin.TrackingUniverseStanding, 0, stationsPose);
+            float yMax = float.MinValue;
+            uint chosenStation = 0;
+            for (uint i = 0; i < Valve.VR.OpenVR.k_unMaxTrackedDeviceCount; i++)
+            {
+                if (Valve.VR.OpenVR.System.GetTrackedDeviceClass(i) == Valve.VR.ETrackedDeviceClass.TrackingReference)
+                {
+                    Vector3 baseStation = stationsPose[i].mDeviceToAbsoluteTracking.GetPosition();
+                    if (yMax == float.MinValue)
+                    {
+                        yMax = baseStation.y;
+                        chosenStation = i;
+                    }
+                    else
+                    {
+                        if (baseStation.y > yMax)
+                        {
+                            chosenStation = i;
+                        }
+                    }
+                }
+            }
+            Vector3 baseStationPos = stationsPose[chosenStation].mDeviceToAbsoluteTracking.GetPosition();
+            Quaternion baseStationRot = stationsPose[chosenStation].mDeviceToAbsoluteTracking.GetRotation();
+
+            baseStationOrigin.transform.SetPositionAndRotation(baseStationPos, baseStationRot);
+
+            //First, upload own headPos and headRot
+            UpdatePosValue(playerNumber, baseStationOrigin.transform.InverseTransformPoint(localRigTrackedCamera.position));
+            UpdateRotValue(playerNumber, Quaternion.Inverse(baseStationOrigin.transform.rotation) * localRigTrackedCamera.transform.rotation);
+            //Then, download other headPos and headRot
+            otherPlayerHead.transform.SetPositionAndRotation(baseStationOrigin.transform.TransformPoint(playersHeadsLocalPositions[(playerNumber + 1) % 2]), baseStationOrigin.transform.rotation * playersHeadsLocalRotations[(playerNumber + 1) % 2]);
+        }
+}
     /// <summary>
     /// Upload local position on server
     /// </summary>
@@ -128,14 +155,14 @@ public class SyncHeads : NetworkBehaviour
     void GetMarker10Rot(Mirror.NetworkConnection target)
     {
         Debug.Log("Uploading marker rot");
-        SetRotation(markerWorldOrigin.transform.rotation);
+        SetRotation(baseStationOrigin.transform.rotation);
     }
 
     [TargetRpc]
     void SetMarker10Transform(Mirror.NetworkConnection target, Vector3 newPos,Quaternion newRot)
     {
         Debug.Log("recieving marker transform");
-        markerWorldOrigin.transform.SetPositionAndRotation(newPos, newRot);
+        baseStationOrigin.transform.SetPositionAndRotation(newPos, newRot);
     }
 
 
