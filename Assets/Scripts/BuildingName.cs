72eca5a8bc1663978dfc7e6975999d87b7b0f9b2
Crenau_Stagiaires Lucas_Detto <ldetto@enseignement.nantes.archi.fr>
2023-01-04 16:59:04 +0100

    Added building names above models

---
+++ Assets/Scripts/BuildingName.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuildingName : MonoBehaviour
+{
+    [SerializeField]
+    private string englishName;
+
+    [SerializeField]
+    private string frenchName;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (GameManager.Instance.languageSetToEnglish)
+        {
+            GetComponent<TMPro.TMP_Text>().text = englishName;
+        }
+        else
+        {
+            GetComponent<TMPro.TMP_Text>().text = frenchName;
+        }
+    }
+
+
+}
