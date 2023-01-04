b288bb60d2397bc09126ff682242abfc13867519
Crenau_Stagiaires Lucas_Detto <ldetto@enseignement.nantes.archi.fr>
2023-01-04 10:49:15 +0100

    Small text fixes

--- Assets/Scripts/TwentyQuestionsTextUpdater.cs
+++ Assets/Scripts/TwentyQuestionsTextUpdater.cs
@@ -41,12 +41,12 @@ public class TwentyQuestionsTextUpdater : MonoBehaviour
         if (languageIsEnglish)
         {
             shownText.text = "Play the 20 questions game, one has a secret word and the other asks up to 20 closed questions (yes/no) to find it, then do it again by reversing roles.";
-            shownText.text += $"Your word is : {GameManager.Instance.TwentyQuestionsWord}.";
+            shownText.text += $" Your word is : {GameManager.Instance.TwentyQuestionsWord}.";
         }
         else
         {
             shownText.text = "Jouez au jeu des 20 questions, l'un a un mot secret et l'autre lui pose jusqu'à 20 questions fermées (oui/non) pour le trouver, puis refaites le en inversant les rôles.";
-            shownText.text += $"Votre mot est : {GameManager.Instance.TwentyQuestionsWord}.";
+            shownText.text += $" Votre mot est : {GameManager.Instance.TwentyQuestionsWord}.";
         }
     }
 
